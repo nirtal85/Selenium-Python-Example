67a1b9cb0abadae8af83abd4de15d0b053447666
unknown <eshorrosh@gmail.com>
2020-10-24 16:42:47 +0300

    add method for screenshot on failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 import pytest
+import allure
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -13,5 +16,25 @@ def create_driver(request):
 
     def kill_driver():
         driver.quit()
+        # outcome = yield
+        # rep = outcome.get_result()
+        # if rep.failed:
+        #     screenshot_name = 'screenshot-%s.png' % datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        #     allure.attach(driver.get_screenshot_as_png(), name=screenshot_name,
+        #                   attachment_type=allure.attachment_type.PNG)
+        # if request.node.rep.failed:
+        #     screenshot_name = 'screenshot-%s.png' % datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        #     allure.attach(driver.get_screenshot_as_png(), name=screenshot_name,
+        #                   attachment_type=allure.attachment_type.PNG)
 
     request.addfinalizer(kill_driver)
+
+    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+    def pytest_runtest_makereport(item):
+        outcome = yield
+        rep = outcome.get_result()
+        if rep.when == "setup" or rep.when == "call":
+            if rep.failed:
+                screenshot_name = 'screenshot-%s.png' % datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+                allure.attach(driver.get_screenshot_as_png(), name=screenshot_name,
+                              attachment_type=allure.attachment_type.PNG)
