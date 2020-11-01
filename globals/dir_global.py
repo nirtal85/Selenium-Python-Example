d8c626a8de218164ee491b89a7b73756ab1c1556
unknown <eshorrosh@gmail.com>
2020-11-01 23:44:06 +0200

    some changes

--- globals/dir_global.py
+++ globals/dir_global.py
@@ -2,4 +2,4 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 INI_CONFIGS_PATH = os.path.join(ROOT_DIR, "ini_configs")
-JSON_FILES_PATH = os.path.join(ROOT_DIR, "json_files")
+JSON_FILES_PATH = os.path.join(ROOT_DIR, "data")
