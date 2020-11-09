d8053d79a6577a1ef00f22b9cecf0b77f0a42578
unknown <eshorrosh@gmail.com>
2020-11-09 09:51:37 +0200

    fix allure environment display

--- globals/dir_global.py
+++ globals/dir_global.py
@@ -4,3 +4,6 @@ ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 INI_CONFIGS_PATH = os.path.join(ROOT_DIR, "ini_configs")
 DATA_FILES_PATH = os.path.join(ROOT_DIR, "data")
 ENV_FILE_PATH = os.path.join(ROOT_DIR, "allure-results")
+# Creating allure-results directory
+if not os.path.exists(ENV_FILE_PATH):
+    os.mkdir(ENV_FILE_PATH)
