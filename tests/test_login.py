ec8d49790592ddddcca6221fb2f9e5a2e043191b
Nir Tal <nirt236@gmail.com>
2022-05-19 23:03:34 +0300

    1

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 import allure
 import pytest
+from assertpy import assert_that
 
 from tests.test_base import BaseTest
 
@@ -15,7 +16,13 @@ users = [
 @pytest.mark.security
 class TestLogin(BaseTest):
 
-    @allure.description("Skip Test example")
-    @allure.title("Skipped test example")
-    def test_skip(self):
-        pass
+    @allure.description("valid login")
+    @allure.title("Login with valid credentials test")
+    @pytest.mark.run(order=1)
+    def test_valid_login(self):
+        username = self.config_reader.config_section_dict("Base Url")["username"]
+        password = self.config_reader.config_section_dict("Base Url")["password"]
+        self.pages['about_page'].click_login_link()
+        self.pages['login_page'].login(username, password)
+        expected_page_title = self.json_reader.read_from_json()["login"]["ws_page_title"]
+        assert_that(expected_page_title).is_equal_to(self.pages['projects_page'].get_title())
