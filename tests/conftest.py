0fa1e8044e24f11fa8bd2b853ce6cacf0d798dd7
Nir Tal <nirt236@gmail.com>
2023-01-30 00:52:08 +0200

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,8 +44,8 @@ def write_allure_environment(prep_properties):
             "Driver_Version": driver.capabilities['browserVersion'],
             "Base_URL": base_url,
             "Commit_Date": datetime.fromtimestamp(repo.head.commit.committed_date).strftime('%c'),
-            "Commit Message": repo.head.commit.message,
-            "Commit Id": repo.head.object.hexsha,
+            "Commit_Message": repo.head.commit.message.strip(),
+            "Commit_Id": repo.head.object.hexsha,
             "Commit_Author_Name": repo.head.commit.author.name,
             "Branch": repo.active_branch.name
         })
