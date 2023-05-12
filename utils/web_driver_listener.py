07ddcce0276cc3f58bded7063cc30d468435dda0
Nir Tal <nirt236@gmail.com>
2023-05-12 23:54:02 +0300

    WIP

--- utils/web_driver_listener.py
+++ utils/web_driver_listener.py
@@ -3,7 +3,9 @@ from typing import Union, List
 from selenium.webdriver import Chrome, Edge, Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class DriverEventListener(AbstractEventListener):
@@ -13,3 +15,7 @@ class DriverEventListener(AbstractEventListener):
         for element in webelements:
             if element.is_displayed():
                 driver.execute_script("arguments[0].setAttribute('style', 'border: 2px solid red;');", element)
+
+    def before_click(self, element: WebElement, driver: Union[Chrome, Firefox, Edge]) -> None:
+        wait = WebDriverWait(driver, 10)
+        wait.until(expected_conditions.element_to_be_clickable(element))
