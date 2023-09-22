998f04f6e63b633049bfbb2d3764e784fcbaab15
nirtal85 <nirt236@gmail.com>
2023-09-23 00:11:47 +0300

    adding vrt

--- tests/visual_test.py
+++ tests/visual_test.py
@@ -1,7 +1,9 @@
 import allure
 import pytest
 from assertpy import assert_that
-from visual_regression_tracker import TestRun, TestRunStatus
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from visual_regression_tracker import IgnoreArea, TestRun, TestRunStatus
 
 from tests.base_test import BaseTest
 
@@ -18,6 +20,38 @@ class TestVisual(BaseTest):
                 TestRun(
                     name="my image",
                     imageBase64=self.driver.get_screenshot_as_base64(),
+                )
+            ).testRunResponse.status.name
+        ).is_equal_to(TestRunStatus.OK)
+
+    @allure.title("Visual test of login page with ignored area")
+    def test_login_visual_with_ignore_area(self, vrt_tracker):
+        element_to_ignore = self.wait.until(
+            expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, "h1"))
+        )
+        location = element_to_ignore.location
+        size = element_to_ignore.size
+        width, height, x, y = (
+            size["width"],
+            size["height"],
+            location["x"],
+            location["y"],
+        )
+        assert_that(
+            vrt_tracker.track(
+                TestRun(
+                    name="my image",
+                    imageBase64=self.driver.get_screenshot_as_base64(),
+                    ignoreAreas=(
+                        [
+                            IgnoreArea(
+                                x=x,
+                                y=y,
+                                width=width,
+                                height=height,
+                            )
+                        ]
+                    ),
                 )
             ).testRunResponse.status.name
         ).is_equal_to(TestRunStatus.OK)
