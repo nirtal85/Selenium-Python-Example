b208dc028be630312964360d466bd53a3ddd29a9
nirtal85 <nirt236@gmail.com>
2024-01-24 10:33:53 +0200

    wip

--- utils/constants.py
+++ utils/constants.py
@@ -5,3 +5,4 @@ class Constants:
     AUTOMATION_USER_AGENT: str = "automation"
     DATA_PATH: Path = Path(Path(__file__).absolute().parent.parent, "data")
     CHROME_DOWNLOAD_DIRECTORY: Path = DATA_PATH / "downloads"
+    DIFF_TOLERANCE_PERCENT: float = 0.01
